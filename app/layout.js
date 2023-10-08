4c30ba6c985c5643d1b0bb5ad05646c8528c6254
Dodjrgskbxhrve <125135685+Dodjrgskbxhrve@users.noreply.github.com>
2023-10-08 15:34:20 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,10 +2,10 @@ import { Analytics } from "@vercel/analytics/react";
 import "../styles/globals.css";
 
 export const metdata = {
-  title: "Чат бот с моделями LlaMa 2",
+  title: "Чатбот LLaMa",
   openGraph: {
-    title: "Llama Чат",
-    description: "Чат бот с моделями LlaMa 2",
+    title: "Чатбот LLaMa",
+    description: "Чатбот LlaMa",
   },
 };
 
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html>
       <head>
-        <title>Чат бот Llama 2</title>
+        <title>Chat with Llama 2</title>
         <link
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🦙</text></svg>"
